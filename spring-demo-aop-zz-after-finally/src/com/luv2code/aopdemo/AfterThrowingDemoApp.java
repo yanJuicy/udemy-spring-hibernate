282522b3f61e908d038ceb8df8baa18fc6487e70
yanJuicy <hanjub3@gmail.com>
2022-03-24 02:27:00 +0900

    AOP @After 타입

---
+++ spring-demo-aop-zz-after-finally/src/com/luv2code/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,42 @@
+package com.luv2code.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+
+public class AfterThrowingDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		List<Account> theAccounts = null;
+		
+		try {
+			// add a boolean flag to simulate exceptions
+			boolean tripWire = true;
+			theAccountDAO.findAccounts(tripWire);
+		} catch (Exception exc) {
+			System.out.println("\n\nMain Program... caught excpetion: " + exc);
+		}
+		
+		System.out.println("\n\nMain Program: AfterReturningDemoApp");
+		System.out.println("----");
+		
+		System.out.println(theAccounts);
+		
+		System.out.println("\n");
+		
+		
+		// close the context
+		context.close();
+	}
+
+}
